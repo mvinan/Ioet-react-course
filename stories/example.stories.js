cf3f9d09887337b2fa41eba0fac3ba6b36a1115f
Miguel Viñan H <mvinan@icloud.com>
2018-03-20 16:29:22 -0500

    FEAT: add new example to stories

---
+++ stories/example.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('Example', module)
+  .add('Basic Example', () => {
+    // Example component Here
+
+    return <span />
+  })
